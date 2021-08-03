0dd82205738f233f48e02aae12bbeee819fb01c8
tangqingguo <49896266+tangqingguo@users.noreply.github.com>
2021-08-03 18:49:20 +0800

    Add files via upload

---
+++ src/main/java/ivan/helloworld/HelloApp.java
@@ -0,0 +1,12 @@
+package ivan.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HelloApp {
+	public static void main(String[] args) {
+        SpringApplication.run(HelloApp.class, args);
+    }
+}
---
+++ src/main/java/ivan/helloworld/controller/HelloController.java
@@ -0,0 +1,14 @@
+package ivan.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping(value="/hello")    
+    public String getHello() {
+		return "hello";
+	}
+
+}
